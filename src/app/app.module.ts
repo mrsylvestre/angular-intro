6f72a5e42c041c857ad9b319e49e189afdc2bf5a
Mathieu <mathieusylvestre17@gmail.com>
2019-02-02 18:52:12 -0500

    section 08 - default pipes and custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardBorderDirective } from './card-border.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, CardBorderDirective],
+  declarations: [AppComponent, CardBorderDirective, PokemonTypeColorPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/dist/app/app.module.js
+++ src/dist/app/app.module.js
@@ -9,13 +9,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
+var card_border_directive_1 = require("./card-border.directive");
+var pokemon_type_color_pipe_1 = require("./pokemon-type-color.pipe");
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent],
+            declarations: [app_component_1.AppComponent, card_border_directive_1.CardBorderDirective, pokemon_type_color_pipe_1.PokemonTypeColorPipe],
             bootstrap: [app_component_1.AppComponent]
         })
     ], AppModule);
