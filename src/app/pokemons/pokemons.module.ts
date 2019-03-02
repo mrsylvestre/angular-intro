447210b63bba548e25b7fc828a59646352741f11
Mathieu <mathieusylvestre17@gmail.com>
2019-03-02 11:39:17 -0500

    injection de service

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -6,18 +6,18 @@ import { DetailPokemonComponent } from './details/details-pokemon.component';
 import { CardBorderDirective } from './card-border.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 import { PokemonRoutingModule } from './pokemons-routing.module';
+import { PokemonsService } from './pokemon.service';
 
 @NgModule({
-	imports: [
-		CommonModule, // default module class that you need to inherit in components attached
-		PokemonRoutingModule
-	],
-	declarations: [
-		ListPokemonsComponent,
-		DetailPokemonComponent,
-		CardBorderDirective,
-		PokemonTypeColorPipe
-	],
-	providers: [] // Providers are the services to use with components attached
+  // default module class that you need to inherit in components attached
+  imports: [CommonModule, PokemonRoutingModule],
+  declarations: [
+    ListPokemonsComponent,
+    DetailPokemonComponent,
+    CardBorderDirective,
+    PokemonTypeColorPipe
+  ],
+  // Providers are the services to use with components attached
+  providers: [PokemonsService]
 })
-export class PokemonsModule { }
+export class PokemonsModule {}
